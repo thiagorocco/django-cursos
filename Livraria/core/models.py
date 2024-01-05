97c7fd22448fb2069bbf7dabea31d6cc13ac0871
thiagorocco <deroccotr@gmail.com>
2024-01-05 06:25:26 -0300

    Realizado o make migrations e migrate para os novos modelos Editora e Categoria

--- Livraria/core/models.py
+++ Livraria/core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=255)
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=255)
+    site = models.URLField()
