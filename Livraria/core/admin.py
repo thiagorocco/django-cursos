c2bdced23e85622c4717e7f463172b9d9efa9746
thiagorocco <deroccotr@gmail.com>
2024-01-05 07:17:02 -0300

    Criação do model Autor e alteração do verbose_name_plural

--- Livraria/core/admin.py
+++ Livraria/core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from core.models import Categoria, Editora
+from core.models import Autor, Categoria, Editora
 
 # Register your models here.
+admin.site.register(Autor)
 admin.site.register(Categoria)
 admin.site.register(Editora)
--- Livraria/core/models.py
+++ Livraria/core/models.py
@@ -13,3 +13,12 @@ class Editora(models.Model):
     
     def __str__(self):
         return self.nome
+    
+class Autor(models.Model):
+    class Meta:
+        verbose_name_plural = "autores"
+        
+    nome = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.nome
