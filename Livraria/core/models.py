c221e8dd226699158512d7b1b3afb23dc08a9edd
thiagorocco <deroccotr@gmail.com>
2024-01-05 07:28:56 -0300

    Model Livro adicionado, porém há erros a serem corrigidos no método add

--- Livraria/core/models.py
+++ Livraria/core/models.py
@@ -22,3 +22,14 @@ class Autor(models.Model):
     
     def __str__(self):
         return self.nome
+
+class Livro(models.Model):
+    titulo = models.CharField(max_length=255)
+    ISBN = models.CharField(max_length=32)
+    quantidade = models.IntegerField()
+    preco = models.FloatField()
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="livros")
+    editora = models.ForeignKey(Editora, on_delete=models.PROTECT, related_name="livros")
+    
+    def __str__(self):
+        return self
