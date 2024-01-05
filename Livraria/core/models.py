5e483fda621cdd8305619a5e848d00f26cd55219
thiagorocco <deroccotr@gmail.com>
2024-01-05 06:33:43 -0300

    Registrados os modelos de dados em admin.py

--- Livraria/core/models.py
+++ Livraria/core/models.py
@@ -3,7 +3,13 @@ from django.db import models
 # Create your models here.
 class Categoria(models.Model):
     descricao = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.descricao
 
 class Editora(models.Model):
     nome = models.CharField(max_length=255)
     site = models.URLField()
+    
+    def __str__(self):
+        return self.nome
