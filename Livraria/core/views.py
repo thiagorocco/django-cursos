5eccbd9494238377b80812dd308a43c757fd5b8a
thiagorocco <deroccotr@gmail.com>
2024-01-26 22:24:45 -0300

    Preparação para eximir o erro do método post

--- Livraria/core/views.py
+++ Livraria/core/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
 from django.views import View
 from core.models import Categoria
 
@@ -10,6 +12,8 @@ def teste(request):
 def teste2(request):
     return HttpResponse('Página 2')
 
+
+@method_decorator(csrf_exempt, name="dispatch")
 class CategoriaView(View):
     def get(self, request):
         data = list(Categoria.objects.values())
