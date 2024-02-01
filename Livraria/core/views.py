72056aaa9d2a05608a400632f0f8a30609babc4f
thiagorocco <deroccotr@gmail.com>
2024-01-31 23:51:14 -0300

    Criação da classe CategoriaDetail

--- Livraria/core/views.py
+++ Livraria/core/views.py
@@ -72,4 +72,11 @@ class CategoriasList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CategoriaDetail(APIView):
+    def get(self, request, id):
+        categoria = get_object_or_404(Categoria.objects.all(), id=id)
+        serializer = CategoriaSerializer(categoria)
+        return Response(serializer.data)
+        
         
