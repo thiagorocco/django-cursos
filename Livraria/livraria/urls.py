8c846e89c2903dfc51af777a4b603ce30d7db1ad
thiagorocco <deroccotr@gmail.com>
2024-02-02 23:21:39 -0300

    Trabalhando com a view ModelViewSet

--- Livraria/livraria/urls.py
+++ Livraria/livraria/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from core import views
+from rest_framework import routers
+
+
+router = routers.DefaultRouter()
+router.register(r'categorias-viewset', views.CategoriaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +33,6 @@ urlpatterns = [
     path('categorias-apiview/',views.CategoriasList.as_view()),
     path('categorias-apiview/<int:id>/',views.CategoriaDetail.as_view()),
     path('categorias-generic/',views.CategoriasListGeneric.as_view()),
-    path('categorias-generic/<int:id>/',views.CategoriaDetailGeneric.as_view())
+    path('categorias-generic/<int:id>/',views.CategoriaDetailGeneric.as_view()),
+    path('',include(router.urls))
 ]
