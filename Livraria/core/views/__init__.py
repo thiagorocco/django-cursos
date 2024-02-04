8ff19e645baed6696abf6005bd6d3a6fcf7ecd36
thiagorocco <deroccotr@gmail.com>
2024-02-04 10:23:09 -0300

    Consertado o erro do carregamento de __init__.py em views

--- Livraria/core/views/__init__.py
+++ Livraria/core/views/__init__.py
@@ -1,5 +1,5 @@
 from .testesIniciais import teste, teste2
 from .categoriaClass import CategoriaView
-from .categoriaAPIView import CategoriaDetail, CategoriasList
+from .categoriaApiView import CategoriaDetail, CategoriasList
 from .categoriaGeneric import CategoriaDetailGeneric, CategoriasListGeneric
 from .categoria import CategoriaViewSet
