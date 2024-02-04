805dd68824f7b333fc3b3b4858b1e6c97e540151
thiagorocco <deroccotr@gmail.com>
2024-02-03 22:50:53 -0300

    Criação do arquivo categoria.py na pasta views. Arquivo views.py apagado

--- Livraria/core/views/categoria.py
+++ Livraria/core/views/categoria.py
@@ -1,6 +1,3 @@
-from django.http import HttpResponse, JsonResponse
-
-from django.views import View
 from core.serializer import CategoriaSerializer
 from rest_framework.viewsets import ModelViewSet
 from core.models import Categoria
@@ -9,4 +6,3 @@ from core.models import Categoria
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
-    
