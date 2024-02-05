b295466e587878866afdedfb76f912da6e9b4b20
thiagorocco <deroccotr@gmail.com>
2024-02-04 21:32:30 -0300

    Importação da classe LivroViewSet no arquivo __init__.py em views

--- Livraria/core/views/__init__.py
+++ Livraria/core/views/__init__.py
@@ -5,3 +5,4 @@ from .categoriaGeneric import CategoriaDetailGeneric, CategoriasListGeneric
 from .categoria import CategoriaViewSet
 from .editora import EditoraViewSet
 from .autor import AutorViewSet
+from .livro import LivroViewSet
