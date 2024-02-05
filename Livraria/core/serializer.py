dc430a88dbed272e3fc0954d93dcd4abb5a57a47
thiagorocco <deroccotr@gmail.com>
2024-02-04 21:28:33 -0300

    Criação de classe serializer para Livro

--- Livraria/core/serializer.py
+++ Livraria/core/serializer.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from core.models import Categoria, Editora, Autor
+from core.models import Categoria, Editora, Autor, Livro
 
 
 class CategoriaSerializer(ModelSerializer):
@@ -17,4 +17,10 @@ class EditoraSerializer(ModelSerializer):
 class AutorSerializer(ModelSerializer):
     class Meta:
         model = Autor
+        fields = '__all__'
+
+
+class LivroSerializer(ModelSerializer):
+    class Meta:
+        model = Livro
         fields = '__all__'
