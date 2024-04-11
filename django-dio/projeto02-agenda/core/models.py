a3ceaf175a8b045a2e08e5ac0878175919394f16
thiagorocco <deroccotr@gmail.com>
2024-04-11 06:46:35 -0300

    Definicao do model Evento em models.py

--- django-dio/projeto02-agenda/core/models.py
+++ django-dio/projeto02-agenda/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Evento(models.Model):
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True, null=True)
+    data_evento = models.DateTimeField()
+    data_cricao = models.DateTimeField(auto_now=True)
