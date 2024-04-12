25c32d3769f2ef486bb10e499e518f955bcae93a
thiagorocco <deroccotr@gmail.com>
2024-04-12 06:48:54 -0300

    alteração do banco de dados e criação da classe modelAdmin EventoAdmin para exibir dados dos campos de forma personalizada

--- django-dio/projeto02-agenda/core/models.py
+++ django-dio/projeto02-agenda/core/models.py
@@ -5,7 +5,7 @@ class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
-    data_cricao = models.DateTimeField(auto_now=True)
+    data_criacao = models.DateTimeField(auto_now=True)
     class Meta:
         db_table = 'evento'
 
