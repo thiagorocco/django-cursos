87b5f5278b948c4baeb58b9962197d71ba5dbdd0
thiagorocco <deroccotr@gmail.com>
2024-04-12 06:36:42 -0300

    Adicionado método __str__ ao model Evento e verbose name ao campo data_evento

--- django-dio/projeto02-agenda/core/models.py
+++ django-dio/projeto02-agenda/core/models.py
@@ -4,7 +4,10 @@ from django.db import models
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
-    data_evento = models.DateTimeField()
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_cricao = models.DateTimeField(auto_now=True)
     class Meta:
         db_table = 'evento'
+
+    def __str__(self):
+        return self.titulo
