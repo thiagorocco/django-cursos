f00a190a166abed484f31180b66b3d67b6120635
thiagorocco <deroccotr@gmail.com>
2024-04-13 11:07:58 -0300

    Criação da view lista_eventos

--- django-dio/projeto02-agenda/core/views.py
+++ django-dio/projeto02-agenda/core/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponse, render
 from core.models import Evento
 
 def titulo(request, id):
     evento = Evento.objects.get(id=id)
     return HttpResponse('<h1>Título do evento: {}</h1><h2>{}</h2>'.format(evento.titulo,evento.data_evento))
+
+def lista_eventos(request):
+    render(request, 'agenda.html')
