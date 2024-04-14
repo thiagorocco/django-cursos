a67e4dc6ec75cfe51e7d3be3f51f11b30fd7dbf1
thiagorocco <deroccotr@gmail.com>
2024-04-13 23:47:38 -0300

    ULR raiz redirecionando para a url agenda/

--- django-dio/projeto02-agenda/agenda/urls.py
+++ django-dio/projeto02-agenda/agenda/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from core import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('eventos/<int:id>', views.titulo),
-    path('agenda/', views.lista_eventos)
+    path('agenda/', views.lista_eventos),
+    path('', RedirectView.as_view(url='/agenda/'))
 ]
