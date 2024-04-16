3bcb62db814024df840d08aaa851de776f0986fd
thiagorocco <deroccotr@gmail.com>
2024-04-15 22:47:51 -0300

    Configurado o form para autenticar usuario cadastrado

--- django-dio/projeto02-agenda/core/views.py
+++ django-dio/projeto02-agenda/core/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponse, render, redirect
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth import authenticate, login
 def login_user(request):
     return render(request, 'login.html')
+
+def submit_login(request):
+    if request.POST:
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        usuario = authenticate(username=username, password=password)
+        if usuario is not None:
+            login(request, usuario)
+            return redirect('/')
+    else:
+        redirect('/')
 @login_required(login_url='/login/')
 def titulo(request, id):
     evento = Evento.objects.get(id=id)
