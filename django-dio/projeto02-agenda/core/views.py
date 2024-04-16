f12ac36b737287e716f02a025b42df7221461016
thiagorocco <deroccotr@gmail.com>
2024-04-15 22:02:35 -0300

    Restringindo a autenticacao

--- django-dio/projeto02-agenda/core/views.py
+++ django-dio/projeto02-agenda/core/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import HttpResponse, render
 from core.models import Evento
+from django.contrib.auth.decorators import login_required
 
 def titulo(request, id):
     evento = Evento.objects.get(id=id)
     return HttpResponse('<h1>Título do evento: {}</h1><h2>{}</h2>'.format(evento.titulo,evento.data_evento))
-
+@login_required()
 def lista_eventos(request):
     evento = Evento.objects.all()
     dados = {'eventos': evento}
