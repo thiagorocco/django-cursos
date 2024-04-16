6cc4e9ecbc77687c0a3063650362d1c23ca6f848
thiagorocco <deroccotr@gmail.com>
2024-04-15 22:29:02 -0300

    Criação de redirecionamento para a página de login quando não estiver autenticado

--- django-dio/projeto02-agenda/core/views.py
+++ django-dio/projeto02-agenda/core/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import HttpResponse, render
 from core.models import Evento
 from django.contrib.auth.decorators import login_required
 
+def login_user(request):
+    return render(request, 'login.html')
+@login_required(login_url='/login/')
 def titulo(request, id):
     evento = Evento.objects.get(id=id)
     return HttpResponse('<h1>Título do evento: {}</h1><h2>{}</h2>'.format(evento.titulo,evento.data_evento))
-@login_required()
+@login_required(login_url='/login/')
 def lista_eventos(request):
     evento = Evento.objects.all()
     dados = {'eventos': evento}
