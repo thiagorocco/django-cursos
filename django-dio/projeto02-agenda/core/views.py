12eaeaa90bf33ef106cf3e6c89f49dbdbf1875e0
thiagorocco <deroccotr@gmail.com>
2024-04-17 22:18:11 -0300

    Formulário de evento funcionando para cadastrar

--- django-dio/projeto02-agenda/core/views.py
+++ django-dio/projeto02-agenda/core/views.py
@@ -35,3 +35,15 @@ def evento(request):
     usuario = request.user
     dados = {'usuario': usuario}
     return render(request, 'evento.html', dados)
+@login_required()
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              data_evento=data_evento,
+                              descricao=descricao,
+                              usuario=usuario)
+    return redirect('/')
