ff88efc695e458d7748ed90e8066c59aee6352c6
thiagorocco <deroccotr@gmail.com>
2024-04-17 22:28:51 -0300

    Modificado a model Evento. Adicionado o campo local e alterado o template evento.html bem como a view submit_evento

--- django-dio/projeto02-agenda/core/models.py
+++ django-dio/projeto02-agenda/core/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
     descricao = models.TextField(blank=True, null=True)
+    local = models.CharField(max_length=100, blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateTimeField(auto_now=True)
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)
