9341e509025fbe0545043e28ac7f4ba0151d7f65
thiagorocco <deroccotr@gmail.com>
2024-05-01 21:52:15 -0300

    Implementação do código viewsets

---
+++ django-e-drf-livraria-basica/books/api/viewsets.py
@@ -0,0 +1,8 @@
+from rest_framework import viewsets
+from books.api import serializers
+from books import models
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    serializer_class = serializers.BooksSerializers
+    queryset = models.Books.objects.all()
