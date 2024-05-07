9158347e6caecec8dee43dc3ccb310bb4ba3c985
thiagorocco <deroccotr@gmail.com>
2024-05-07 07:09:46 -0300

    Alterações no projeto, proteção de senha em arquivo externo e com gitignore

--- apiErp/core/settings.py
+++ apiErp/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from core import password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'curso_sistema_erp',
         'USER': 'root',
-        'PASSWORD': 't*197346',
+        'PASSWORD': password.PASSWORD,
         'PORT': 3306,
     }
 }
