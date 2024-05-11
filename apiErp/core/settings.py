eb6eaba0c9c44f24e1ebd0fbd5372ad617c60a0a
thiagorocco <deroccotr@gmail.com>
2024-05-11 00:21:55 -0300

    Concluida a configuração do jwt em settings.py

--- apiErp/core/settings.py
+++ apiErp/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 from core import password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -135,4 +136,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
+}
+
+SIMPLE_JWT = {
+    'SLIDING_TOKEN_LIFETIME': timedelta(days=1)
 }
