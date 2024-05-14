e85e3701309bf68485238cd6df914d52141df2f4
thiagorocco <deroccotr@gmail.com>
2024-05-14 06:56:51 -0300

    Criação da model Enterprise referente ao app companies

--- apiErp/companies/models.py
+++ apiErp/companies/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from accounts.models import User
+
 
 # Create your models here.
+class Enterprise(models.Model):
+    name = models.CharField(max_length=175)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #integridade referencial
