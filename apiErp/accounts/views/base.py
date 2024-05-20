f86295f62d049de69d068cab006e952ff516838f
thiagorocco <deroccotr@gmail.com>
2024-05-20 07:13:59 -0300

    Concluida a view Base

--- apiErp/accounts/views/base.py
+++ apiErp/accounts/views/base.py
@@ -1,6 +1,7 @@
 from rest_framework import APIView
 from rest_framework.exceptions import APIException
 from companies.models import Enterprise, Employee
+from accounts.models import User_Groups, Group_Permissions
 
 
 class Base(APIView):
@@ -13,9 +14,21 @@ class Base(APIView):
             user_id=user_id).exists()
         # Se for dono já retorna a empresa, pois um dono já possui todas as permissões
         if enterprise['is_owner']:
-            return enterprise
-    
+            return enterprise    
         # Permissions, Get Employee
         employee = Employee.objects.filter(user_id=user_id).first()
         if not employee:
             raise APIException("Este usuário não é um funcionário")
+
+        groups = User_Groups.objects.filter(user_id=user_id).all()
+
+        for g in groups:
+            group = g.group
+            permissions = Group_Permissions.objects.filter(group_id=group.id).all()
+            for p in permissions:
+                enterprise['permissions'].append({
+                    "id": p.permission.id,
+                    "label": p.permission.name,
+                    "codename": p.permission.codename
+                })
+        return enterprise
